2508682654977a6b989bc895a84538182aee4eda
DeedleFake <deedlefake@users.noreply.github.com>
2026-02-22 17:56:47 -0500

    use a `form` and add a `location` filter

--- lib/pinventory_web/live/items_live.ex
+++ lib/pinventory_web/live/items_live.ex
@@ -7,7 +7,10 @@ defmodule PinventoryWeb.ItemsLive do
   def render(assigns) do
     ~H"""
     <Layouts.app flash={@flash}>
-      <.input type="text" value={@query} phx-change="change_query" placeholder="Filter..." />
+      <.form for={@query} class="flex flex-row gap-1" phx-change="search" phx-submit="search">
+        <.input type="text" field={@query[:q]} wrapperclass="flex-1" placeholder="Filter..." />
+        <.input type="select" field={@query[:location]} prompt="Location..." options={[]} />
+      </.form>
       <div
         id="items"
         class="flex flex-col bg-white text-black rounded-xl cursor-default select-none"
@@ -22,31 +25,31 @@ defmodule PinventoryWeb.ItemsLive do
   end
 
   @impl true
-  def handle_params(%{"q" => q}, _uri, socket) when q != "" do
-    socket =
-      socket
-      |> stream(:items, Items.list_items(filter: q))
-      |> assign(:query, q)
-
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_params(_params, _uri, socket) do
+  def handle_params(params, _uri, socket) do
     socket =
       socket
       |> stream(:items, Items.list_items())
-      |> assign(:query, "")
+      |> assign(:query, to_form(params))
 
     {:noreply, socket}
   end
 
   @impl true
-  def handle_event("change_query", %{"query" => query}, socket) do
+  def handle_event("search", params, socket) do
+    query = to_query(params)
+
     socket =
       socket
-      |> push_patch(~p"/?q=#{query}")
+      |> push_patch(to: ~p"/?#{query}")
 
     {:noreply, socket}
+  end
+
+  defp to_query(params) do
+    keys = [:location, :q]
+
+    for key <- keys, val = params[to_string(key)], val != "" do
+      {key, val}
+    end
   end
 end
